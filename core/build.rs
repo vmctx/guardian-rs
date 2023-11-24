5273d15ce38d60c402989285e413b71c96f01fc0
joshuа <41162644+felix-rs@users.noreply.github.com>
2023-11-24 05:45:50 +0100

    maybe finally fixed build script for github actions

--- core/build.rs
+++ core/build.rs
@@ -5,12 +5,19 @@ fn main() {
     let path = env::var("CARGO_MANIFEST_DIR").unwrap().replace(
         "\\core", "\\vm-build",
     );
-    panic!("{:?}", env::vars());
-    let status = Command::new("cargo").env_clear()
-        .env("Path", env::var("Path").unwrap())
-        .env("TMP", env::var("TMP").unwrap())
-        .env("TEMP", env::var("TEMP").unwrap())
-        .current_dir(path.clone())
+    let mut cargo_vars = Vec::new();
+    for (var, _) in env::vars() {
+        if var.to_ascii_lowercase().starts_with("cargo_") {
+            cargo_vars.push(var);
+        }
+    }
+    let mut command = Command::new("cargo");
+
+    for var in cargo_vars {
+        command.env_remove(var);
+    }
+
+    let status = command.current_dir(path.clone())
         .args(&["b", "--release", "--target", "x86_64-pc-windows-msvc"])
         .status().unwrap();
     assert!(status.success(), "could not build vm");
