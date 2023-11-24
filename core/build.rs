ab4d954ec01cb15debaf74f91683ad68fea27022
joshuа <41162644+felix-rs@users.noreply.github.com>
2023-11-24 05:08:18 +0100

    build script fixes

--- core/build.rs
+++ core/build.rs
@@ -5,12 +5,12 @@ fn main() {
     let path = env::var("CARGO_MANIFEST_DIR").unwrap().replace(
         "\\core", "\\vm-build",
     );
-    Command::new("cargo").env_clear()
+    let status = Command::new("cargo").env_clear()
         .env("Path", env::var("Path").unwrap())
         .env("TMP", env::var("TMP").unwrap())
         .env("TEMP", env::var("TEMP").unwrap())
         .current_dir(path.clone())
         .args(&["b", "--release", "--target", "x86_64-pc-windows-msvc"])
         .status().unwrap();
-      panic!("{:?}", path);
+    assert!(status.success(), "could not build vm");
 }
