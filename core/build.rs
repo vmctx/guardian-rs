828e471e59a9ade4b3bd2b5606841f19bb9f4891
joshuа <41162644+felix-rs@users.noreply.github.com>
2023-11-24 05:51:40 +0100

    cleaned up build script

--- core/build.rs
+++ core/build.rs
@@ -2,22 +2,20 @@ use std::process::Command;
 use std::env;
 
 fn main() {
-    let path = env::var("CARGO_MANIFEST_DIR").unwrap().replace(
-        "\\core", "\\vm-build",
-    );
     let mut cargo_vars = Vec::new();
-    for (var, _) in env::vars() {
-        if var.to_ascii_lowercase().starts_with("cargo_") {
-            cargo_vars.push(var);
-        }
-    }
+    env::vars().into_iter().for_each(|(key, _)| if key.starts_with("CARGO_") {
+        cargo_vars.push(key);
+    });
+
     let mut command = Command::new("cargo");
 
     for var in cargo_vars {
         command.env_remove(var);
     }
 
-    let status = command.current_dir(path.clone())
+    let path = env::var("CARGO_MANIFEST_DIR").unwrap()
+        .replace("\\core", "\\vm-build", );
+    let status = command.current_dir(path)
         .args(&["b", "--release", "--target", "x86_64-pc-windows-msvc"])
         .status().unwrap();
     assert!(status.success(), "could not build vm");
